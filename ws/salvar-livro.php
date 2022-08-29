38270baae91bea5f44b005c66f2f49a2b10246d8
Livia Pietra <108291565+liviapietra@users.noreply.github.com>
2022-08-29 18:18:44 +0000

    Initial commit

---
+++ ws/salvar-livro.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once "../model/Conexao.php";
+
+$nome_livro = $_GET["txt_livro"];
+$sql = "INSERT INTO public.book
+(nome, paginas, autor)
+VALUES('$nome_livro', 100, '???');";
+
+
+Conexao:: exec($sql);
+
+header("Location: ../");
+
+
+
+
+
+
+
+
